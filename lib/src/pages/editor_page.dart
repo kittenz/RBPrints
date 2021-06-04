4b78e79063a9af181beb8df5a41bd0f637c67a47
Roni Nevalainen <kitten@kittenz.dev>
2021-06-04 11:14:26 +0300

    app: right click context menu for adding nodes

--- lib/src/pages/editor_page.dart
+++ lib/src/pages/editor_page.dart
@@ -1,8 +1,26 @@
 import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 import '../widgets/node_widget.dart';
 
 class EditorPage extends StatelessWidget {
+  void _onPointerDown(PointerDownEvent event, BuildContext context) async {
+    if (event.kind == PointerDeviceKind.mouse && event.buttons == kSecondaryMouseButton) {
+      final menuItem = await showMenu<int>(
+          context: context,
+          items: [
+            PopupMenuItem(child: Text('Node 1'), value: 1),
+            PopupMenuItem(child: Text('Node 2'), value: 2),
+          ],
+          position: RelativeRect.fromSize(event.position & Size(48.0, 48.0), (context.findRenderObject() as RenderBox).size));
+
+      switch (menuItem) {
+        default:
+          break;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BackdropScaffold(
@@ -14,10 +32,13 @@ class EditorPage extends StatelessWidget {
       frontLayer: Row(
         children: [
           Expanded(
-            child: Stack(
-              children: [
-                Node(nodeType: NodeType.control),
-              ],
+            child: Listener(
+              onPointerDown: (PointerDownEvent event) => {
+                _onPointerDown(event, context),
+              },
+              child: Container(
+                color: Colors.white, // Doesn't work without color: for some reason
+              ),
             ),
             flex: 5,
           ),
