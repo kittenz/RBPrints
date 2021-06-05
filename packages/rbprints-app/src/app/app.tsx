9a2c370619e521c82082a4b37e69dffbd57a3bae
Roni Nevalainen <kitten@kittenz.dev>
2021-06-05 21:00:35 +0300

    New codebase based on roblox-ts and React/Electron

---
+++ packages/rbprints-app/src/app/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../scss/styles.scss';
+
+function App() {
+	return (
+		<h1>Hello, World!</h1>
+	);
+}
+
+export default App;
---
+++ packages/rbprints-app/src/pages/start-server/start-server.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StartServerPage() {
+
+}
+
+export default StartServerPage;
